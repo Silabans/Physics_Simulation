5658bccfa52e45452ac1fbc370df51638762e3a5
Matthew <matthew.yedija@gmail.com>
2026-08-01 22:30:54 +0800

    remove detect_collision function and integrate it into the resolve_circle_collision function

--- Source/forces.hpp
+++ Source/forces.hpp
@@ -14,52 +14,6 @@ inline Vector2D operator+(const Vector2D& a, const Vector2D& b) { return {a.x + 
 inline Vector2D operator*(const Vector2D& a, const Vector2D& b) { return {a.x * b.x, a.y * b.y}; }
 inline float dot(const Vector2D& a, const Vector2D& b) { return a.x * b.x + a.y * b.y; }
 
-// contanct forces
-bool detect_collision(RigidBody& A, RigidBody& B) {
-    if (A.getShape()->type == ShapeType::CIRCLE && B.getShape()->type == ShapeType::CIRCLE) {
-        const Circle* circleA = static_cast<const Circle*>(A.getShape());
-        const Circle* circleB = static_cast<const Circle*>(B.getShape());
-
-        float ra = circleA->radius;
-        float rb = circleB->radius;
-
-        Vector2D Apos = A.getPosition();
-        Vector2D Bpos = B.getPosition();
-
-        Vector2D delta = Apos - Bpos;
-        float square_dist = dot(delta, delta);
-
-        float overlap = squaring(rb + ra) - square_dist;
-
-        if (overlap > 0) {
-            float total_radius = ra + rb;
-            float x = total_radius - std::abs(delta.x);
-            float y = total_radius - std::abs(delta.y);
-
-            float restoration_x = (Apos.x > Bpos.x) ? x : -x;
-            float restoration_y = (Apos.y > Bpos.y) ? y : -y;
-
-            if (std::abs(delta.x) < total_radius) {
-                A.setPositionX(Apos.x + restoration_x);
-                B.setPositionX(Bpos.x - restoration_x);
-            }
-
-            if (std::abs(delta.y) < total_radius) {
-                A.setPositionY(Apos.y + restoration_y);
-                B.setPositionY(Bpos.y - restoration_y);
-            }
-
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    // implement other types of collision (different shapes)
-    return false;
-}
-
 
 Vector2D calculate_vrel(const RigidBody& a, const RigidBody& b) {
     // Impulse
@@ -89,7 +43,7 @@ void resolve_circle_collision(RigidBody& a, RigidBody& b) {
 
     if (square_dist >= squaring(total_radius)) return;
 
-    
+
 
 
     float e = 0.80f; // coefficient of restitution
